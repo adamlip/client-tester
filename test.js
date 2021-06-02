737adb7395cc48668be186df7e76337f31bb18e2
Adam Lipthay-Eotvos <adamlip@gmail.com>
2021-06-02 19:28:18 +0200

    sdf

--- test.js
+++ test.js
@@ -1,7 +1,7 @@
 var pug = require('pug')
 var axios = require('axios')
 var fs = require('fs')
-let puggy = pug.compileFile("./costes_booking_confirmed.pug")
+let puggy = pug.compileFile("./Default_new_order_customer.pug")
 
 //let apiToken = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJkZXZpY2VVVUlEIjoiIiwicGxheWVySUQiOm51bGwsInVzZXJVVUlEIjoiMTFFQjkwODM2MDRENUI1MEJCQTVDMTFDOEVBMTVBOUYiLCJ0b2tlblR5cGUiOiJ1c2VyIiwidG9rZW5VVUlEIjoiMTFlYmFmOGJiNGI0YjlmMDhjNjQ4MTA3MDEyYzMyNmMiLCJpYXQiOjE2MjA0MzAwMTMsImV4cCI6MTYyMzAyMjAxM30.ihlVh9SRb6Vvc4pQx70HvZhHW6XrsbCrXE7uMDGqfpo'
 let apiToken = ''
@@ -14,28 +14,33 @@ axios.interceptors.request.use(config => {
     return config
 })
 
-/*axios('https://pipi3131.okeoke.io/api/v1/order/getOrder', {      
+axios('https://pnrvqvmru.okeoke.io/api/v1/order/getOrder', {      
     method: 'post',
     data: {
-        "orderUUID": "11EBBEC71EA6F680AFE2C126F13A46A0", 
+        "orderUUID": "11EBC2D2D7152520A37267082F9D32C6", 
         "informational": true
     }
-}).then(res => {
-    console.log(res.data)
-    
-    let data = puggy({order: res.data})
-    
-    fs.writeFile('test.html', data, function (err) {
-        if (err) return console.log(err);
-    });
-})*/
-
-axios.get('https://booking.okeoke.io/api/v1/booking/uuid/11EBBEDFC1C85C109409CDF27524625E', { }).then(res => {
-    console.log(res.data.data)
-    let moment = require('moment')
-    let data = puggy({booking: res.data.data, moment})
-    
-    fs.writeFile('test.html', data, function (err) {
-        if (err) return console.log(err);
-    });
+}).then(order => {
+    console.log(order.data)
+    axios('https://pnrvqvmru.okeoke.io/api/v1/location/one/' + order.data.locationUUID, {      
+    method: 'get'
+    }).then(location => {
+        console.log(JSON.stringify(location.data, null, 2))
+        
+        let data = puggy({order: order.data, location: location.data})
+        
+        fs.writeFile('test.html', data, function (err) {
+            if (err) return console.log(err);
+        });
+    })
 })
+
+// axios.get('https://booking.okeoke.io/api/v1/booking/uuid/11EBBEDFC1C85C109409CDF27524625E', { }).then(res => {
+//     console.log(res.data.data)
+//     let moment = require('moment')
+//     let data = puggy({booking: res.data.data, moment})
+    
+//     fs.writeFile('test.html', data, function (err) {
+//         if (err) return console.log(err);
+//     });
+// })

53045cf42062e62a8c9048f034dde51262adf0b4
Adam Lipthay-Eotvos <adamlip@gmail.com>
2021-06-02 23:41:23 +0200

    added default payment reminder

--- convert.js
+++ convert.js
@@ -1,6 +1,6 @@
 var fs = require('fs')
 
-fs.readFile("./defaults/order_status_ready.pug", 'utf8', function (err, data) {
+fs.readFile("./defaults/order_payment_reminder.pug", 'utf8', function (err, data) {
     if (err) return console.log(err)
     let words = data.replace(/\r?\n|\r/g, "\\n\\r");
     //console.log(words)
